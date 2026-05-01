35e22083c0c396e759d4cabe224bf68ff9943254
James McGlashan <164065467+JLMJLMJLM14159@users.noreply.github.com>
2026-05-01 14:04:33 +0100

    idk

---
+++ Grid.cs
@@ -0,0 +1,60 @@
+﻿using VectorInt;
+using MemoryPack;
+using System.Collections;
+
+namespace Vertex
+{
+    [MemoryPackable]
+    public partial class Grid<T>((VectorInt2, VectorInt2)? minMaxBounds) : IEnumerable<KeyValuePair<VectorInt2, T>>
+    {
+        public (VectorInt2, VectorInt2)? MinMaxBounds { get; set; } = minMaxBounds; //MIN IS INCLUSIVE, MAX IS EXCLUSIVE
+        private Dictionary<VectorInt2, T> GridDictionary { get; set; } = [];
+
+        public T this[VectorInt2 key]
+        {
+            get
+            {
+                if (InBounds(key))
+                { 
+                    if (GridDictionary.TryGetValue(key, out T? value)) { return value!; }
+                    else { throw new($"Value from key {key} does not exist."); } 
+                }
+                else
+                { throw new($"Key exceeds inclusive lower bound or exclusive upper bound. Can't be bothered to tell you though so figure it out yourself. You're welcome :) Ok fine I'll tell you. Your key is {key} and your lower and upper bounds are {MinMaxBounds}. Now you're properly welcome :)"); }
+            }
+            set
+            {
+                if (InBounds(key))
+                { GridDictionary[key] = value; }
+                else
+                { throw new($"Key exceeds inclusive lower bound or exclusive upper bound. Can't be bothered to tell you though so figure it out yourself. You're welcome :) Ok fine I'll tell you. Your key is {key} and your lower and upper bounds are {MinMaxBounds}. Now you're properly welcome :)"); }
+            }
+        }
+
+        public void Remove(VectorInt2 vectorInt2) => GridDictionary.Remove(vectorInt2);
+
+        public static implicit operator Dictionary<VectorInt2, T>(Grid<T> grid) => grid.GridDictionary;
+
+        public IEnumerator<KeyValuePair<VectorInt2, T>> GetEnumerator()
+        { foreach (KeyValuePair<VectorInt2, T> kvp in GridDictionary) { yield return kvp; } }
+
+        IEnumerator<KeyValuePair<VectorInt2, T>> IEnumerable<KeyValuePair<VectorInt2, T>>.GetEnumerator() => GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
+        public static bool FirstVectorInt2IsCloserToOriginThanSecondVectorInt2(VectorInt2 v1, VectorInt2 v2) => int.Max(int.Abs(v1.X), int.Abs(v1.Y)) < int.Max(int.Abs(v2.X), int.Abs(v2.Y));
+        public static bool FirstVectorInt2IsEquallyCloseToOriginThanSecondVectorInt2(VectorInt2 v1, VectorInt2 v2) => int.Max(int.Abs(v1.X), int.Abs(v1.Y)) == int.Max(int.Abs(v2.X), int.Abs(v2.Y));
+        public static bool FirstVectorInt2IsFutherAwayFromOriginThanSecondVectorInt2(VectorInt2 v1, VectorInt2 v2) => int.Max(int.Abs(v1.X), int.Abs(v1.Y)) > int.Max(int.Abs(v2.X), int.Abs(v2.Y));
+
+        public bool InBounds(VectorInt2 key)
+        {
+            if (MinMaxBounds != null)
+            {
+                return key.X >= MinMaxBounds!.Value.Item1.X &&
+                    key.X < MinMaxBounds!.Value.Item2.X &&
+                    key.Y >= MinMaxBounds!.Value.Item1.Y &&
+                    key.Y < MinMaxBounds!.Value.Item2.Y;
+            }
+            else { return true; }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # Vertex
-A simple grid system for C#.
+
+Just a simple grid system in C#, used for a few of my projects. Uses VectorInt so no floats. The reference to MemoryPack is just to make it `[MemoryPackable]`, and it does nothing on its own, so don't worry about it.
+
+Uses an MIT liscence, so feel free to use and edit! ([Github project is here](https://github.com/JLMJLMJLM14159/Vertex))
+
+## Documentation
+
+### Creation
+
+To create a `Grid`, simply use
+
+```csharp
+Grid<int> grid = new(null);
+```
+
+(Replace `int` with whatever type you want to store in the grid, and `null` with a `(VectorInt2, VectorInt2)` for the inclusive minimum bounds and exclusive maximum bounds of the grid if you want, but you can keep it borderless.)
+
+### Usage
+
+To add, change and remove stuff from a `Grid`, do:
+
+```csharp
+grid[new(4, 4)] = 3;
+grid.Remove(new(4, 4));
+```
+
+You can check the distance of different points from the origin (what "ring" they are in around the origin):
+
+```csharp
+bool b1 = Grid.FirstVectorInt2IsCloserToOriginThanSecondVectorInt2(v1, v2);
+bool b2 = Grid.FirstVectorInt2IsEquallyCloseToOriginThanSecondVectorInt2(v1, v2);
+bool b3 = Grid.FirstVectorInt2IsFutherAwayFromOriginThanSecondVectorInt2(v1, v2);
+```
+
+(I know, catchy.)
+
+You can check if a coordinate is within the bounds of your `Grid` by doing:
+
+```csharp
+bool b = grid.InBounds(v);
+```
+
+(Will return true if your `Grid` has no bounds.)
+
+You can cycle through KVPs with foreach:
+
+```csharp
+foreach (KeyValuePair<VectorInt2, int> kvp in grid) ...
+```
+
+You can implicitly convert the `Grid` to a `Dictionary`:
+
+```csharp
+(Dictionary<VectorInt2, int>)grid ...
+```
